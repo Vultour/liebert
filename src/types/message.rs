e3a441cd4a4b8ebb38ae50cf74052ea1c13f18fa
vultour <noreply@matxel.sk>
2017-03-12 20:46:15 +0000

    Update base Message type

--- src/types/message.rs
+++ src/types/message.rs
@@ -4,11 +4,14 @@ use std::sync;
 
 
 pub enum Message{
-    Data(String),
+    Data(String, i64, String),
+    Format(String, Vec<::agent::plugins::Format>),
     Shutdown(String),
 
     Fatal(String),
 
+    LogError(String),
+    LogWarn(String),
     LogInfo(String),
     LogDebug(String)
 }
@@ -21,9 +24,12 @@ pub type MessageReceiver    = sync::mpsc::Receiver<Message>;
 impl Message{
     pub fn get_type(&self) -> String{
         match self{
-            &Message::Data(_)       => return String::from("Data"),
+            &Message::Data(_, _, _) => return String::from("Data"),
+            &Message::Format(_, _)  => return String::from("Format"),
             &Message::Shutdown(_)   => return String::from("shutdown"),
             &Message::Fatal(_)      => return String::from("Fatal"),
+            &Message::LogError(_)   => return String::from("LogError"),
+            &Message::LogWarn(_)    => return String::from("LogWarn"),
             &Message::LogInfo(_)    => return String::from("LogInfo"),
             &Message::LogDebug(_)   => return String::from("LogDebug")
         }
@@ -31,11 +37,14 @@ impl Message{
 
     pub fn get_content(&self) -> String{
         match self{
-            &Message::Data(ref m)       => return m.clone(),
-            &Message::Shutdown(ref m)   => return m.clone(),
-            &Message::Fatal(ref m)      => return m.clone(),
-            &Message::LogInfo(ref m)    => return m.clone(),
-            &Message::LogDebug(ref m)   => return m.clone()
+            &Message::Data(ref n, ref t, ref m) => return format!("Data for {} [{}]: {}", n, t, m),
+            &Message::Format(ref n, ref m)      => return format!("Format for {}, length {}", n, m.len()),
+            &Message::Shutdown(ref m)           => return m.clone(),
+            &Message::Fatal(ref m)              => return m.clone(),
+            &Message::LogError(ref m)           => return m.clone(),
+            &Message::LogWarn(ref m)            => return m.clone(),
+            &Message::LogInfo(ref m)            => return m.clone(),
+            &Message::LogDebug(ref m)           => return m.clone()
         }
     }
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,4 +1,5 @@
 use std::sync;
+use std::thread;
 use std::collections::HashMap;
 
 mod message;
@@ -11,6 +12,6 @@ pub use self::message::MessageReceiver;
 
 
 pub type ConfigurationMap       = HashMap<String, String>;
-pub type ConfigurationMutex     = sync::Arc<sync::Mutex<ConfigurationMap>>;
+pub type ConfigurationMutex     = sync::Arc<sync::Mutex<ConfigurationMap>>; // TODO: Change Mutex to RwLock
 
-pub type NamedSenderHashMap     = HashMap<String, sync::mpsc::Sender<Message>>;
+pub type NamedSenderHashMap     = HashMap<String, (sync::mpsc::Sender<Message>, thread::JoinHandle<()>)>;
